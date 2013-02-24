5b90444d653ab5a6bbe89c0fc518012501fab753
Andrew Francis <aprancis@gmail.com>
2013-02-24 15:48:22 +0000

    adding logging to show what port we're listening on

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var app = express();
 // serve static files for the demp from /public
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8009);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
